7ef5ed257ff7115cebc82b37c73b396c77faf00a
kappeguss <mazzolenikevin11@gmail.com>
2024-11-22 09:01:08 +0100

    Skibidi

--- eCommerce_Mazzoleni/Form1.Designer.cs
+++ eCommerce_Mazzoleni/Form1.Designer.cs
@@ -28,12 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            button1 = new Button();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(112, 94);
+            button1.Name = "button1";
+            button1.Size = new Size(120, 75);
+            button1.TabIndex = 0;
+            button1.Text = "Visualizza Prodotti";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(112, 246);
+            button2.Name = "button2";
+            button2.Size = new Size(120, 75);
+            button2.TabIndex = 1;
+            button2.Text = "Visualizza Totale";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button button1;
+        private Button button2;
     }
 }
--- eCommerce_Mazzoleni/Form1.cs
+++ eCommerce_Mazzoleni/Form1.cs
@@ -6,5 +6,10 @@ namespace eCommerce_Mazzoleni
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
